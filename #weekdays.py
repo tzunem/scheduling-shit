da4cd1e059aefe3a5effc85f446b6b260ef1b05d
tzunem <120443785+tzunem@users.noreply.github.com>
2023-01-24 15:35:40 -0800

    Add files via upload

---
+++ #weekdays.py
@@ -0,0 +1,17 @@
+    #weekdays
+monday = [8.30, 3.12, 4.00, 6.30, 8.30]
+tuesday = [8.30, 2.20, 3.00, 6.30, 8.30]
+wednesday = [8.30, 3.12, 4.00, 6.30, 8.30]
+thursday = [8.30, 2.20, 3.00, 6.30, 8.30]
+friday = [8.30, 3.12, 4.00, 7.00, 9.30]
+
+    #weekend
+saturday = [9.00-10.00, 11.30, 5.00, 10.00]
+sunday = [9.00-10.00, 11.00, 5.00, 8.30]
+    #extra days off (monday off etc.)
+sunday_extra_day = [9.00-10.00, 11.30, 5.00, 10.00] #same as saturday schedules
+extra_day_off = [9.00-10.00, 11.00, 5.00, 8.30]
+    #actual breaks (spring break etc.)
+breakdays = [9.00-10.00, 11.30, 5.00, 10.00]
+lastbreakday = [9.00-10.00, 11.00, 5.00, 8.30]
+
