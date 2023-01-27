0ef03f3878571480a47b739fb4a9a3d14dab645f
Your Name <you@example.com>
2023-01-26 18:16:20 -0800

    stuff

--- #weekdays.py
+++ #weekdays.py
@@ -9,14 +9,14 @@ thursday = [8.30, 2.20, 3.00, 6.30, 8.30]
 friday = [8.30, 3.12, 4.00, 7.00, 9.30]
 
     #weekend
-saturday = [9.00-10.00, 11.30, 5.00, 10.00]
-sunday = [9.00-10.00, 11.00, 5.00, 8.30]
+saturday = [9.00, 10.00, 11.30, 5.00, 10.00]
+sunday = [9.00, 10.00, 11.00, 5.00, 8.30]
     #extra days off (monday off etc.)
-sunday_extra_day = [9.00-10.00, 11.30, 5.00, 10.00] #same as saturday schedules
-extra_day_off = [9.00-10.00, 11.00, 5.00, 8.30]
+sunday_extra_day = [9.00, 10.00, 11.30, 5.00, 10.00] #same as saturday schedules
+extra_day_off = [9.00, 10.00, 11.00, 5.00, 8.30]
     #actual breaks (spring break etc.)
-breakdays = [9.00-10.00, 11.30, 5.00, 10.00]
-lastbreakday = [9.00-10.00, 11.00, 5.00, 8.30]
+breakdays = [9.00, 10.00, 11.30, 5.00, 10.00]
+lastbreakday = [9.00, 10.00, 11.00, 5.00, 8.30]
 
 #Lineup 
 weekdays = [monday, tuesday, wednesday, thursday, friday]
